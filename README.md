bb601c4ecabcda8f7fc09e859731bd0e4a001fbd
YogeshBorole1998 <borole116@gmail.com>
2022-08-24 14:49:14 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-sfpzln
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-sfpzln)
---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'hello-world',
+  template: `
+  <h2>Hello Yogesh..!!</h2>
+  <p> I am a Software Engg. in capgemini<p>
+  `
+})
+export class AppComponent  {
+}
